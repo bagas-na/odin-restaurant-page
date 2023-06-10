682a42d7740220ad815065f78f95dbabf3212ce1
bagas-na <bagasnurseno@gmail.com>
2023-06-10 08:41:42 +0700

    Add asset management, output management, and development configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: "./src/index.js",
+    home: "./src/home.js",
+    menu: "./src/menu.js",
+    contact: "./src/contact.js",
   },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Asset, Output, and Development Management",
+    }),
+  ],
+  output: {
+    filename: "[name].main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  }
 };
