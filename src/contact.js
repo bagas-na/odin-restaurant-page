32add05ac77b1639addabde58a5c21fc3e78824b
bagas-na <bagasnurseno@gmail.com>
2023-06-16 20:29:19 +0700

    Change embedded map to not use API key, and use google maps shared link instead

--- src/contact.js
+++ src/contact.js
@@ -40,7 +40,10 @@ const embedMap = () => {
   map.setAttribute("loading", "lazy");
   map.setAttribute("allowfullscreen", "");
   map.setAttribute("referrerrpolicy", "no-referrer-when-downgrade");
-  map.setAttribute("src", "https://www.google.com/maps/embed/v1/place?key=AIzaSyAGtHqF8CGT6Gd6mK8vfaoejPbmYfX2_8s&q=Estancia+Jakarta");
+  map.setAttribute(
+    "src",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7633.197679161514!2d106.83571522953801!3d-6.206203057287077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5ef5379f887%3A0x90147184ecc50573!2sEstancia%20Coffee%20n%20Cafe!5e0!3m2!1sen!2sid!4v1686921885877!5m2!1sen!2sid"
+  );
 
   embedMap.appendChild(map);
 
@@ -48,27 +51,27 @@ const embedMap = () => {
 };
 
 const contactCard = () => {
-    const contactCard = document.createElement('div');
-    contactCard.classList.add('contact-card');
+  const contactCard = document.createElement("div");
+  contactCard.classList.add("contact-card");
 
-    contactCard.append(contactInfo(), embedMap());
+  contactCard.append(contactInfo(), embedMap());
 
-    return contactCard;
-}
+  return contactCard;
+};
 
 const contact = () => {
-    const contact = document.createElement('div');
-    contact.classList.add('contact');
+  const contact = document.createElement("div");
+  contact.classList.add("contact");
 
-    const header1 = document.createElement('h1');
-    header1.textContent = "Contact Us";
+  const header1 = document.createElement("h1");
+  header1.textContent = "Contact Us";
 
-    const header2 = document.createElement('h2');
-    header2.textContent = "Get in Touch";
+  const header2 = document.createElement("h2");
+  header2.textContent = "Get in Touch";
 
-    contact.append(header1, header2, contactCard());
+  contact.append(header1, header2, contactCard());
 
-    return contact;
-}
+  return contact;
+};
 
 export default contact;
